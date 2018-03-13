bacfe1ebe1506e8ca87f14dec6c9f14523983945
Skylor <theoriginalopticgamer@gmail.com>
2018-03-13 14:36:18 -0500

    Update Village.java

--- Village.java
+++ Village.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project01;
 
 /**
  *
  * @author Skylor Criqui
- * @version 1
  */
+
 public class Village {
     private int population;
     private int livestock;

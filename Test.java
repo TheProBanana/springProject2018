0f7a6bc74cf526bd3ed1a33ccbca4b494984317b
Skylor <theoriginalopticgamer@gmail.com>
2018-03-13 14:33:25 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project01;
 
 /**
  *
- * @author skycr
+ * @author Skylor Criqui
  */
+
 public class Test {
     public static void main(String[] args){
         Map map1 = new Map(100,100);

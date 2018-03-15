995efaf2eb1e00a82b421e825569ad5f14cb0dbe
Skylor <theoriginalopticgamer@gmail.com>
2018-03-15 14:05:20 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,19 +1,27 @@
 package project01;
 
+import javax.swing.*;
+
 /**
  *
  * @author Skylor Criqui
  */
 
-public class Test {
+public class Test extends JPanel {
     public static void main(String[] args){
-        Map map1 = new Map(100,100);
+    // Window framework
+        JFrame frame = new JFrame("Space Game");
+        frame.add(new Test());
+        frame.setSize(1080, 720);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        /*Map map1 = new Map(100,100);
         Map map2 = new Map(100, 100);
         System.out.println("Map One:");
         map1.mapConstructor();
         map1.getMap();
         System.out.println("Map Two:");
         map2.mapConstructor();
-        map2.getMap();
+        map2.getMap();*/
     }
 }

6efef74f07f164aae1182fdf6b90d47002190a0e
Ally8 <37390336+Ally8@users.noreply.github.com>
2018-05-01 20:39:04 -0500

    Add files via upload

---
+++ CreatePlanet.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SpaceGame;
+
+/**
+ *
+ * @author Allison Cross
+ */
+public class CreatePlanet{
+    private final int radius = 1; //set defaults for no arg
+    private String name = " ";
+    private String  item1 = " ";
+    private String item2= " ";
+    
+    public CreatePlanet() {}
+    
+    //public void setRadius(int radius) {
+    //    this.radius = radius;
+    //}
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setItem1(String item){
+        this.item1 = item;
+    }
+    public void setItem2(String item){
+        this.item2 = item;
+    }
+    public int getRadius(){
+        return radius;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getItem1(){
+        return item1;
+    }
+    public String getItem2(){
+        return item2;
+    }
+}
